ca2f59ec2a9033d16d2c2c93aa36011e70ef4865
Crowdbotics <team@crowdbotics.com>
2024-01-05 07:10:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-shadow-45289",
+  url: "https://red-shadow-45289.botics.co",
+  api: "https://red-shadow-45289.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
